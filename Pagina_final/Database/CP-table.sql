d9f84f728cd426b7d1f5455c710e9b878f015a4a
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-16 23:53:34 +0100

    Update CP-table.sql

--- Pagina_final/Database/CP-table.sql
+++ Pagina_final/Database/CP-table.sql
@@ -1,5 +1,5 @@
 USE pacoparty;
 CREATE TABLE codipostal (
-    ID int auto_increment,
-    CP varchar(50) NOT NULL
+    ID int auto_increment primary key,
+    CP varchar(50)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
