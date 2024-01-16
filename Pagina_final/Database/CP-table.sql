d8f35ad06bb42c94202f6bf7d3d15f02b6d7d5a5
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-16 22:15:34 +0100

    mod sql

--- Pagina_final/Database/CP-table.sql
+++ Pagina_final/Database/CP-table.sql
@@ -1,8 +1,5 @@
 USE pacoparty;
 CREATE TABLE codipostal (
-  ID int(10) UNSIGNED NOT NULL,
-  CP decimal(5,0) NOT NULL,
-  calle varchar(100) DEFAULT NULL,
-  poblacion varchar(100) NOT NULL,
-  provincia varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    ID int(10) auto_increment,
+    CP decimal(5, 0) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
