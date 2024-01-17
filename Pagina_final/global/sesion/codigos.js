fbea87a1d579702b575d4d60d0521e588f10d99c
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-17 12:48:15 +0100

    sesion avanzada

--- Pagina_final/global/sesion/codigos.js
+++ Pagina_final/global/sesion/codigos.js
@@ -1,15 +1,46 @@
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
+document.addEventListener('copy', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
+document.addEventListener('cut', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
+
 function toogle() {
     var blur = document.getElementById('blur');
     blur.classList.toggle('active');
 }
 
-document.addEventListener('contextmenu',function(e){e.preventDefault();e.stopPropagation();});
-document.addEventListener('copy',function(e){e.preventDefault();e.stopPropagation();});
-document.addEventListener('cut',function(e){e.preventDefault();e.stopPropagation();});
+function cambiarSiguienteBotonVerde() {
+    // Obtén el siguiente botón con la clase fa-regular fa-circle
+    const regularButton = document.querySelector('.fa-regular.fa-circle');
+
+    // Si hay un botón, cámbiale la clase y el color
+    if (regularButton) {
+        regularButton.className = 'fa-solid fa-circle-xmark';
+        regularButton.style.color = 'green';
+    }
+}
 
 function numerosRandom() {
+    console.log("numerosRandom function called");
     const buttonContainer = document.getElementById("button-container");
     const buttons = Array.from(buttonContainer.children);
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const value = button.id;
+            console.log(value);
+            
+            // Llama a la función para cambiar el siguiente botón a verde
+            cambiarSiguienteBotonVerde();
+        });
+    });
 
     for (let i = buttons.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -24,8 +55,15 @@ function numerosRandom() {
 }
 
 function pinRandom() {
+    console.log("pinRandom function called");
     const buttonContainer = document.getElementById("pin-btn");
     const buttons = Array.from(buttonContainer.children);
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const value = button.textContent.trim();
+        });
+    });
 
     for (let i = buttons.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
