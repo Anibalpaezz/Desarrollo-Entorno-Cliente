c4a1086211f0ae5026f431b5e8bd828238d3e0cc
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-30 13:35:16 +0100

    pagina servicios

--- Pagina_final/loader/script.js
+++
@@ -1,7 +0,0 @@
-function showLoader() {
-    document.getElementById('loader').style.display = "block";
-
-    setTimeout(function () {
-        window.location.href = '../principal/index.html';
-    }, 3000);
-}
