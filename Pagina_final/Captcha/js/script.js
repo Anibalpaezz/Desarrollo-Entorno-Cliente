a9ce15be1bde745001da2c4f9777c465209102cb
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-02-26 10:23:11 +0100

    captcha fotos

--- Pagina_final/Captcha/js/script.js
+++ Pagina_final/Captcha/js/script.js
@@ -3,9 +3,9 @@ const imagenes = [
     '../Images/captcha/2.jpg',
     '../Images/captcha/3.jpg',
     '../Images/captcha/4.jpg',
-    '../Images/captcha/5.jpg',
-    '../Images/captcha/6.jpg',
-    '../Images/captcha/7.jpg',
+    '../Images/captcha/5.jpeg',
+    '../Images/captcha/6.jpeg',
+    '../Images/captcha/7.jpeg',
     '../Images/captcha/8.jpg',
     '../Images/captcha/9.jpg'
 ];
@@ -49,16 +49,16 @@ function asignarTema() {
 
     switch (tema) {
         case 'bicis':
-            respuestasCorrectas = ['4.jpg', '6.jpg', '8.jpg'];
-            tituloCaptcha.innerText = 'Selecciona las bicicletas correctas:';
+            respuestasCorrectas = ['7.jpg', '8.jpg', '9.jpg'];
+            tituloCaptcha.innerText = 'Selecciona los pcs correctos:';
             break;
         case 'trap':
             respuestasCorrectas = ['1.jpg', '2.jpg', '3.jpg'];
-            tituloCaptcha.innerText = 'Selecciona las imágenes de trap correctas:';
+            tituloCaptcha.innerText = 'Selecciona los clasicos correctos:';
             break;
         case 'semaforos':
-            respuestasCorrectas = ['5.jpg', '7.jpg', '9.jpg'];
-            tituloCaptcha.innerText = 'Selecciona los semáforos correctos:';
+            respuestasCorrectas = ['4.jpg', '5.jpg', '6.jpg'];
+            tituloCaptcha.innerText = 'Selecciona las motos correctas:';
             break;
         default:
             break;
